038aed8b5a93ce94f7d11f20cc355f56bf60dfa9
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2023-06-13 10:57:40 +0200

    UPT lecture DF

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,6 +1,19 @@
 from datetime import datetime
 from flask import Flask, render_template
 from . import app
+import pandas as pd
+from os import environ
+from sqlalchemy import create_engine
+
+def recup_data(url):
+    print('recup', url)
+    df = pd.read_csv(url)
+    print(df.head())
+    print(df.shape)
+
+URL='https://data.seattle.gov/api/views/2bpz-gwpy/rows.csv?accessType=DOWNLOAD' # 2016
+URL='https://data.seattle.gov/api/views/qxjw-iwsh/rows.csv?accessType=DOWNLOAD' # 2017
+recup_data(URL)
 
 @app.route("/")
 def home():
